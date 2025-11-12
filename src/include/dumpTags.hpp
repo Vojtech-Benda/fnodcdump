46de46e24ccb6e19eb3c5bb556c3b97ce8d6f71b
Vojtěch-Benda <vojtech.benda@atlas.cz>
2025-11-12 11:12:18 +0100

    added custom struct formatted

--- src/include/dumpTags.hpp
+++ src/include/dumpTags.hpp
@@ -1,6 +1,8 @@
 #include <unordered_map>
 #include <vector>
 
+#include "fmt/format.h"
+
 #include "dcmtk/dcmdata/dctag.h"
 #include "dcmtk/ofstd/ofcond.h"
 
@@ -8,8 +10,29 @@ enum class E_Dump_Level { STUDY, SERIES };
 
 struct Tag {
   DcmTag dcm_tag{};
+  std::string tag_name{};
   std::string value{};
 };
+
+namespace fmt {
+template <> class fmt::formatter<Tag> {
+
+public:
+  constexpr auto parse(format_parse_context &ctx) {
+    auto i = ctx.begin(), end = ctx.end();
+
+    if (i != end && *i != '}') {
+      throw format_error("invalid format");
+    }
+    return i;
+  }
+
+  template <typename FmtContext>
+  constexpr auto format(const Tag &_tag, FmtContext &ctx) const {
+    return fmt::format_to(ctx.out(), "{}", _tag.tag_name);
+  }
+};
+} // namespace fmt
 
 // key: Series/StudyInstanceUID, values: vector of (TagName, TagValue)
 using TagsMap = std::unordered_map<std::string, std::vector<Tag>>;
